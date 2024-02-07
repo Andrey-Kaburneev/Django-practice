cb13730c91326e86c5dea1ad5801d72a64a19ea3
Андрей Кабурнеев <kaburneew@yandex.ru>
2024-02-07 20:06:43 +0500

    change views

--- django_practice/homepage/views.py
+++ django_practice/homepage/views.py
@@ -1,7 +1,8 @@
-# from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render
+# from django.http import HttpResponse
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse('<h1>Главная страница!</h1>')
+    template_name = 'homepage/index.html'
+    return render(request, template_name)
