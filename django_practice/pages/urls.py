a2d2c2fd9d7fead687debf0dcbd24249b978f2bf
Андрей Кабурнеев <kaburneew@yandex.ru>
2024-02-13 00:57:25 +0500

    create name and namespace

--- django_practice/pages/urls.py
+++ django_practice/pages/urls.py
@@ -1,12 +1,18 @@
 from django.urls import path
 from . import views
 
+app_name = 'pages'
+
 urlpatterns = [
-    path('integer/', views.integer_info),
-    path('collections/', views.collection_info),
-    path('string/', views.string_info),
-    path('boolean/', views.boolean_info),
-    path('data_structures/', views.data_structures_info),
-    path('algorithms/', views.algorithms_info),
-    path('method/', views.method_info),
+    path('integer/', views.integer_info, name='integer_info'),
+    path('collections/', views.collection_info, name='collection_info'),
+    path('string/', views.string_info, name='string_info'),
+    path('boolean/', views.boolean_info, name='boolean_info'),
+    path(
+        'data_structures/',
+        views.data_structures_info,
+        name='data_structures_info'
+        ),
+    path('algorithms/', views.algorithms_info, name='algorithms_info'),
+    path('method/', views.method_info, name='method_info'),
 ]
