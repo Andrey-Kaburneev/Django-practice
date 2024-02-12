6b99e8dde96aea695a7783fe5c4b582a5834f9db
Андрей Кабурнеев <kaburneew@yandex.ru>
2024-02-13 00:56:56 +0500

    create name and namespace

--- django_practice/homepage/views.py
+++ django_practice/homepage/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
+import random
 # from django.http import HttpResponse
 # Create your views here.
 
 
 def index(request):
+    temas = ['1', '2', '3', '4', '5']
+    uniq = random.choice(temas)
     template_name = 'homepage/index.html'
-    return render(request, template_name)
+    context = {
+        'uniq': uniq
+    }
+    return render(request, template_name, context)
